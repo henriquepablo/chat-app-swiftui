10492364aeaa44978f82acc61b296320f0c5c8d0
henriquepablo <pablo269704@gmail.com>
2025-02-01 11:07:59 -0300

    Initial Commit

---
+++ SwiftUIChatApp/SwiftUIChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatAppApp.swift
+//  SwiftUIChatApp
+//
+//  Created by pablo henrique on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIChatAppTests/SwiftUIChatAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatAppTests.swift
+//  SwiftUIChatAppTests
+//
+//  Created by pablo henrique on 01/02/25.
+//
+
+import Testing
+@testable import SwiftUIChatApp
+
+struct SwiftUIChatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
